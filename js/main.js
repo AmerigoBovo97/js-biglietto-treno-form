10f2b372f91db5de8d29b7032fb7d71a2f73de38
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-10 00:50:35 +0200

    feat: add result visualizer

--- js/main.js
+++ js/main.js
@@ -5,22 +5,23 @@ const discount65 = 40;
 const ageDiscount18 = 18;
 const ageDiscount65 = 65;
 let totalPrice;
-const distance = 0;
-const age = 0;
+let distance 
+let age
 let finalResult;
 
 // html variables
 const myForm = document.getElementById("calculator-form");
-const myKms = document.getElementById("mks");
+const myKms = document.getElementById("kms");
 const myAge = document.getElementById("age");
+const showResult = document.getElementById("result")
 
 
 // logic
- 
 myForm.addEventListener("submit", function(event){
 
     event.preventDefault();
 
+    console.log(myKms.value)
     distance = myKms.value;
     totalPrice = kmPrice * distance;
     age = myAge.value;
@@ -31,6 +32,11 @@ myForm.addEventListener("submit", function(event){
         totalPrice = totalPrice * discount65 / 100;
    }
 
-   totalPrice = totalPrice.toFixed(2)
+   totalPrice = totalPrice.toFixed(2);
+
+   showResult.classList.remove("d-none");
+   showResult.innerHTML = `<div class="fw-bold fs-3">Incredibile, ti toccherà pagare addirittura ${totalPrice}£, è assurdo diventerai un senzatetto!!</div>`
+
 
 });
+
